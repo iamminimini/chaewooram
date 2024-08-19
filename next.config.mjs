6e08fad379118d71b7084b79ccd6b659f80ad9e4
이채은 <loev1021@gmail.com>
2024-08-19 13:29:32 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // 개발 중 오류 감지 및 코드 품질 향상을 위해 활성화
-  swcMinify: true, // SWC를 사용한 코드 미니파이
+  swcMinify: false, // SWC를 사용한 코드 미니파이
   images: {
     remotePatterns: [
       {
