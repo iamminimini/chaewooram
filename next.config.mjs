40506fc2d8654192c9818d72d1689293f2dcc5b3
이채은 <loev1021@gmail.com>
2024-08-20 10:49:14 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // 개발 중 오류 감지 및 코드 품질 향상을 위해 활성화
-  swcMinify: false, // SWC를 사용한 코드 미니파이
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'nrs.harvard.edu',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  compiler: {
-    styledComponents: true, // styled-components 서버 사이드 렌더링 지원
-  },
-  // output: 'export',
-  productionBrowserSourceMaps: true,
+  // reactStrictMode: true, // 개발 중 오류 감지 및 코드 품질 향상을 위해 활성화
+  // swcMinify: false, // SWC를 사용한 코드 미니파이
+  // images: {
+  //   unoptimized: true,
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'nrs.harvard.edu',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
+  // compiler: {
+  //   styledComponents: true, // styled-components 서버 사이드 렌더링 지원
+  // },
+  // // output: 'export',
+  // productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
